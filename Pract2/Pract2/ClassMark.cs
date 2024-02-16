e663a7917f3d25a5a24624ba3aa14e3eb8cab384
Den0l <denol02.02.06@gmail.com>
2024-02-16 23:04:21 +0300

    Com

---
+++ Pract2/Pract2/ClassMark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract2
+{
+    internal class ClassMark
+    {
+        public string Name { get; set; }
+        public string Mark { get; set; }
+        public DateTime DateMark { get; set; }
+
+        public ClassMark(string Name, string Mark, DateTime DateMark)
+        {
+            this.Name = Name;
+            this.Mark = Mark;
+            this.DateMark = Convert.ToDateTime(DateMark.ToShortDateString());
+        }
+
+    }
+}
---
+++ Pract2/Pract2/Json.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.IO;
+using System.Numerics;
+
+namespace Pract2
+{
+    internal class Json
+    {
+        static public string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        static public void Serilaz<T>(T Marks)
+        {
+            string json = JsonConvert.SerializeObject(Marks);
+            File.WriteAllText(desktop + "\\Mark.json", json);
+        }
+        static public T Deserilaz<T>()
+        {
+            string json = File.ReadAllText(desktop + "\\Mark.json");
+            T Marks = JsonConvert.DeserializeObject<T>(json);
+            return Marks;
+        }
+    }
+}
